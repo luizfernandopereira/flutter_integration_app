2b3f8b198fb29bcb4b73c39d4316f0f661ac8808
Luiz Fernando Pereira <luiz.pereira@amcom.com.br>
2024-02-05 09:16:43 -0300

    Primeiro commit

---
+++ integration_test/counter_test.dart
@@ -0,0 +1,29 @@
+import 'package:app_test/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('tap on the floating action button, verify counter',
+      (tester) async {
+    // Load app widget.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify the counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+
+    // Finds the floating action button to tap on.
+    final fab = find.byKey(const Key('increment'));
+
+    // Emulate a tap on the floating action button.
+    await tester.tap(fab);
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    // Trigger a frame.
+    await tester.pumpAndSettle();
+
+    // Verify the counter increments by 1.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
